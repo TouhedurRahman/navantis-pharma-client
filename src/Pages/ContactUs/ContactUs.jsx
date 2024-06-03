341bd1f36424ba27e76eba91cb56b37f386b0ca6
TouhedurRahman <86222384+TouhedurRahman@users.noreply.github.com>
2024-06-03 14:30:32 +0600

    query section added in contact us page

--- src/Pages/ContactUs/ContactUs.jsx
+++ src/Pages/ContactUs/ContactUs.jsx
@@ -1,9 +1,43 @@
-import { MdAlternateEmail } from 'react-icons/md';
-import CoverBanner from '../../Components/CoverBanner/CoverBanner';
-import { FiPhoneCall } from 'react-icons/fi';
-import { FaLocationDot } from 'react-icons/fa6';
+import CoverBanner from "../../Components/CoverBanner/CoverBanner";
+import { MdAlternateEmail } from "react-icons/md";
+import { FiPhoneCall } from "react-icons/fi";
+import { useForm } from 'react-hook-form';
+import { AiOutlineMail, AiOutlinePhone } from 'react-icons/ai';
+import { SiNamecheap } from "react-icons/si";
+import { FaLocationDot } from "react-icons/fa6";
+import Swal from "sweetalert2";
+import './ContactUs.css';
 
 const ContactUs = () => {
+    const { register, handleSubmit, reset, formState: { errors } } = useForm();
+
+    const onSubmit = data => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, Submit!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                // get the data
+                console.log(data);
+
+                // reset form
+                reset();
+
+                Swal.fire({
+                    title: "Submitted!",
+                    text: "We reply you as soon as possible.",
+                    icon: "success"
+                });
+            }
+        });
+
+    };
+
     return (
         <div>
             <CoverBanner
@@ -72,6 +106,107 @@ const ContactUs = () => {
                             Birulia Road, Savar,<br />
                             Dhaka-1340, Bangladesh <br />
                         </address>
+                    </div>
+                </div>
+                <div className='my-3 flex flex-col lg:flex-row-reverse justify-center lg:justify-between items-center mt-10'>
+                    <div>
+                        <div className=" avatar">
+                            <div className="w-96 h-96 rounded">
+                                <img src="https://i.ibb.co/NNPhf7w/Contact-Corner.jpg" />
+                            </div>
+                        </div>
+                    </div>
+                    <div className='w-full mx-2'>
+                        <p className='font-bold text-xl text-gray-600 text-center font-serif mb-3'>If any query feel free to knock us</p>
+                        <form onSubmit={handleSubmit(onSubmit)}
+                            className=''
+                        >
+                            <div className="mb-4 relative">
+                                {/* <label htmlFor="name" className="block text-sm font-medium text-gray-700">
+                                    Name
+                                </label> */}
+                                <div className='relative'>
+                                    <SiNamecheap className="absolute top-3 left-3 text-gray-400" />
+                                    <input
+                                        type="text"
+                                        id="name"
+                                        {...register("name", { required: true })}
+                                        className={`mt-1 p-2 pl-10 w-full ${errors.name ? 'border-red-500' : 'border-gray-300'} rounded-md focus:ring-indigo-50 border-2 border-[#1e1d5d] focus:outline-none focus:border-[#FB923C]`}
+                                        placeholder="Your Name"
+                                    />
+                                </div>
+                                {errors.name && <span className="text-red-500 text-sm">Name is required</span>}
+                            </div>
+                            <div className="mb-4 relative">
+                                {/* <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+                                    Email
+                                </label> */}
+                                <div className="relative">
+                                    <AiOutlineMail className="absolute top-3 left-3 text-gray-400" data-aos="fade-up" />
+                                    <input
+                                        type="email"
+                                        id="email"
+                                        {...register("email", { required: true })}
+                                        className={`mt-1 p-2 pl-10 w-full ${errors.email ? 'border-red-500' : 'border-gray-300'} rounded-md focus:ring-indigo-500 border-2 border-[#1e1d5d] focus:outline-none focus:border-[#FB923C]`}
+                                        placeholder="Your Email"
+
+                                    />
+                                </div>
+                                {errors.email && <span className="text-red-500 text-sm">Email is required</span>}
+                            </div>
+                            <div className="mb-4 relative">
+                                {/* <label htmlFor="phone" className="block text-sm font-medium text-gray-700">
+                                    Phone
+                                </label> */}
+                                <div className="relative flex justify-between items-center">
+                                    <AiOutlinePhone className="absolute top-3 left-3 text-gray-400" data-aos="fade-up" />
+                                    <input
+                                        type="tel"
+                                        id="phone"
+                                        {...register("phone", { required: true })}
+                                        className="mt-1 p-2 pl-10 w-full rounded-md focus:ring-indigo-500 border-2 border-[#1e1d5d] focus:outline-none focus:border-[#FB923C]"
+                                        placeholder="Your Phone"
+                                    />
+                                </div>
+                                {errors.phone && <span className="text-red-500 text-sm">Phone no. is required</span>}
+                            </div>
+                            <div className="mb-4">
+                                {/* <label htmlFor="message" className="block text-sm font-medium text-gray-700">
+                                    Message
+                                </label> */}
+                                <textarea
+                                    id="message"
+                                    rows="4"
+                                    {...register("message", { required: true })}
+                                    className={`mt-1 p-2 w-full ${errors.message ? 'border-red-500' : 'border-gray-300'} rounded-md focus:ring-indigo-500 border-2 border-[#1e1d5d] focus:outline-none focus:border-[#FB923C]`}
+                                    placeholder="Your Message"
+
+                                ></textarea>
+                                {errors.message && <span className="text-red-500 text-sm">Message is required</span>}
+                            </div>
+                            {/* <button
+                                type="submit"
+                                className="bg-indigo-500 text-white py-2 px-4 rounded-md hover:bg-indigo-600 transition duration-300"
+                            >
+                                Submit
+                            </button> */}
+                            <div className='w-full text-center'>
+                                <button className="animated-button">
+                                    <svg viewBox="0 0 24 24" className="arr-2" xmlns="http://www.w3.org/2000/svg">
+                                        <path
+                                            d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"
+                                        ></path>
+                                    </svg>
+                                    <span className="text">Submit</span>
+                                    <span className="circle"></span>
+                                    <svg viewBox="0 0 24 24" className="arr-1  text-white" xmlns="http://www.w3.org/2000/svg">
+                                        <path
+                                            d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"
+                                        ></path>
+                                    </svg>
+                                </button>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
