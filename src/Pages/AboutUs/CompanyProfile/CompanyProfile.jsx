571cdeb88aba631d60151c8697bdfe87eddf2f39
TouhedurRahman <86222384+TouhedurRahman@users.noreply.github.com>
2024-06-05 16:29:02 +0600

    about us page responsiveness updated

--- src/Pages/AboutUs/CompanyProfile/CompanyProfile.jsx
+++ src/Pages/AboutUs/CompanyProfile/CompanyProfile.jsx
@@ -2,14 +2,14 @@ const CompanyProfile = () => {
     return (
         <div className='px-1 lg:px-5 lg:border-l-2 lg:border-[#0B5F82]'>
             <div data-aos="fade-down" data-aos-duration="2000">
-                <div className='mt-10 lg:mt-0 text-center md:text-left'>
+                <div className='mt-10 md:mt-0 text-center md:text-left'>
                     <p className='text-xl  font-sans font-extralight'>
                         ABOUT
                     </p>
                     <p className='text-2xl text-[#0B5F82] font-serif font-extrabold'>
                         NAVANTIS PHARMA LIMITED
                     </p>
-                    <hr className='mt-3 w-36 mx-auto lg:mx-0 border-2 border-[#FB923C] rounded' />
+                    <hr className='mt-3 w-36 mx-auto md:mx-0 border-2 border-[#FB923C] rounded' />
                 </div>
 
                 <div className='mt-5' data-aos="fade-left" data-aos-duration="2000">
