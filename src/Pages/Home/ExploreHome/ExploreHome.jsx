7d14f85f6512cb2a340732d232d716750dfced24
TouhedurRahman <86222384+TouhedurRahman@users.noreply.github.com>
2024-06-08 11:54:23 +0600

    explore section updated in home page

--- src/Pages/Home/ExploreHome/ExploreHome.jsx
+++ src/Pages/Home/ExploreHome/ExploreHome.jsx
@@ -1,218 +1,58 @@
-import './ExploreHome.css';
-import { Link } from 'react-router-dom';
-
 const ExploreHome = () => {
+    const contents = [
+        {
+            _id: 1,
+            img: "https://i.ibb.co/DKkk4tm/health-cycle.jpg",
+            title: "Health",
+            subtitle: ""
+        },
+        {
+            _id: 2,
+            img: "https://i.ibb.co/vmKwpT0/happiness-family.jpg",
+            title: "Happiness",
+            subtitle: ""
+        },
+        {
+            _id: 3,
+            img: "https://i.ibb.co/pfSPzJB/beauty-face.jpg",
+            title: "Beauty",
+            subtitle: ""
+        }
+    ]
+
     return (
-        <div className="px-3 lg:px-10 my-5 mb-10 lg:mb-0">
-            <div className="w-full flex flex-col-reverse lg:flex-row justify-center lg:justify-between items-center">
-                <div className="lg:w-[90%]">
-                    <h1 className="flex justify-start items-center text-3xl font-bold text-[#0B5F82]"
+        <div className="px-3 lg:px-10 my-5 mt-24 mb-10">
+            <div className="w-full flex flex-col lg:flex-row justify-center lg:justify-between items-center">
+                <div className="lg:w-full lg:text-center ">
+                    <h1 className="text-3xl font-bold text-[#0B5F82]"
                         data-aos="fade-right"
                         data-aos-duration="2000"
                     >
-                        <span className="avatar w-8 h-16 rounded">
+                        {/* <span className="avatar w-8 h-16 rounded">
                             <img src="https://i.ibb.co/4d7LsvY/navantis-3d-logo.gif" className="w-full h-full object-cover" alt="Avatar Image" />
-                        </span>
-                        <span>Explore</span>
+                        </span> */}
+                        <span>Explore Navantis</span>
+                        <div className='flex justify-center items-center'>
+                            <hr className='mt-3 w-36 border-2 border-[#FB923C] rounded' />
+                        </div>
                     </h1>
-                    <p className='my-5 text-justify' data-aos="fade-left" data-aos-duration="2000">
+                    <p className='my-5 text-center' data-aos="fade-left" data-aos-duration="2000">
                         Explore the World of Skincare with Navantis Pharma Limited. Dive into our curated collection of articles, guides, and expert tips to unlock the secrets of radiant, healthy skin. Discover the latest trends, innovations, and insights that empower you to make informed choices for your skincare journey. From personalized routines to industry breakthroughs, navigate through our Explore section and embark on a transformative experience towards your best skin yet.
                     </p>
-                    <Link to="/explore" className='lg:text-center'>
-                        <button className='explorebtn'>
-                            Explore More
-                        </button>
-                    </Link>
-                </div>
-                <div className="w-full lg:w-1/2 flex justify-center items-center">
-                    <div className="socket">
-                        <div className="gel center-gel">
-                            <div className="hex-brick h1"></div>
-                            <div className="hex-brick h2"></div>
-                            <div className="hex-brick h3"></div>
-                        </div>
-                        <div className="gel c1 r1">
-                            <div className="hex-brick h1"></div>
-                            <div className="hex-brick h2"></div>
-                            <div className="hex-brick h3"></div>
-                        </div>
-                        <div className="gel c2 r1">
-                            <div className="hex-brick h1"></div>
-                            <div className="hex-brick h2"></div>
-                            <div className="hex-brick h3"></div>
-                        </div>
-                        <div className="gel c3 r1">
-                            <div className="hex-brick h1"></div>
-                            <div className="hex-brick h2"></div>
-                            <div className="hex-brick h3"></div>
-                        </div>
-                        <div className="gel c4 r1">
-                            <div className="hex-brick h1"></div>
-                            <div className="hex-brick h2"></div>
-                            <div className="hex-brick h3"></div>
-                        </div>
-                        <div className="gel c5 r1">
-                            <div className="hex-brick h1"></div>
-                            <div className="hex-brick h2"></div>
-                            <div className="hex-brick h3"></div>
-                        </div>
-                        <div className="gel c6 r1">
-                            <div className="hex-brick h1"></div>
-                            <div className="hex-brick h2"></div>
-                            <div className="hex-brick h3"></div>
-                        </div>
-
-                        <div className="gel c7 r2">
-                            <div className="hex-brick h1"></div>
-                            <div className="hex-brick h2"></div>
-                            <div className="hex-brick h3"></div>
-                        </div>
-
-                        <div className="gel c8 r2">
-                            <div className="hex-brick h1"></div>
-                            <div className="hex-brick h2"></div>
-                            <div className="hex-brick h3"></div>
-                        </div>
-                        <div className="gel c9 r2">
-                            <div className="hex-brick h1"></div>
-                            <div className="hex-brick h2"></div>
-                            <div className="hex-brick h3"></div>
-                        </div>
-                        <div className="gel c10 r2">
-                            <div className="hex-brick h1"></div>
-                            <div className="hex-brick h2"></div>
-                            <div className="hex-brick h3"></div>
-                        </div>
-                        <div className="gel c11 r2">
-                            <div className="hex-brick h1"></div>
-                            <div className="hex-brick h2"></div>
-                            <div className="hex-brick h3"></div>
-                        </div>
-                        <div className="gel c12 r2">
-                            <div className="hex-brick h1"></div>
-                            <div className="hex-brick h2"></div>
-                            <div className="hex-brick h3"></div>
-                        </div>
-                        <div className="gel c13 r2">
-                            <div className="hex-brick h1"></div>
-                            <div className="hex-brick h2"></div>
-                            <div className="hex-brick h3"></div>
-                        </div>
-                        <div className="gel c14 r2">
-                            <div className="hex-brick h1"></div>
-                            <div className="hex-brick h2"></div>
-                            <div className="hex-brick h3"></div>
-                        </div>
-                        <div className="gel c15 r2">
-                            <div className="hex-brick h1"></div>
-                            <div className="hex-brick h2"></div>
-                            <div className="hex-brick h3"></div>
-                        </div>
-                        <div className="gel c16 r2">
-                            <div className="hex-brick h1"></div>
-                            <div className="hex-brick h2"></div>
-                            <div className="hex-brick h3"></div>
-                        </div>
-                        <div className="gel c17 r2">
-                            <div className="hex-brick h1"></div>
-                            <div className="hex-brick h2"></div>
-                            <div className="hex-brick h3"></div>
-                        </div>
-                        <div className="gel c18 r2">
-                            <div className="hex-brick h1"></div>
-                            <div className="hex-brick h2"></div>
-                            <div className="hex-brick h3"></div>
-                        </div>
-                        <div className="gel c19 r3">
-                            <div className="hex-brick h1"></div>
-                            <div className="hex-brick h2"></div>
-                            <div className="hex-brick h3"></div>
-                        </div>
-                        <div className="gel c20 r3">
-                            <div className="hex-brick h1"></div>
-                            <div className="hex-brick h2"></div>
-                            <div className="hex-brick h3"></div>
-                        </div>
-                        <div className="gel c21 r3">
-                            <div className="hex-brick h1"></div>
-                            <div className="hex-brick h2"></div>
-                            <div className="hex-brick h3"></div>
-                        </div>
-                        <div className="gel c22 r3">
-                            <div className="hex-brick h1"></div>
-                            <div className="hex-brick h2"></div>
-                            <div className="hex-brick h3"></div>
-                        </div>
-                        <div className="gel c23 r3">
-                            <div className="hex-brick h1"></div>
-                            <div className="hex-brick h2"></div>
-                            <div className="hex-brick h3"></div>
-                        </div>
-                        <div className="gel c24 r3">
-                            <div className="hex-brick h1"></div>
-                            <div className="hex-brick h2"></div>
-                            <div className="hex-brick h3"></div>
-                        </div>
-                        <div className="gel c25 r3">
-                            <div className="hex-brick h1"></div>
-                            <div className="hex-brick h2"></div>
-                            <div className="hex-brick h3"></div>
-                        </div>
-                        <div className="gel c26 r3">
-                            <div className="hex-brick h1"></div>
-                            <div className="hex-brick h2"></div>
-                            <div className="hex-brick h3"></div>
-                        </div>
-                        <div className="gel c28 r3">
-                            <div className="hex-brick h1"></div>
-                            <div className="hex-brick h2"></div>
-                            <div className="hex-brick h3"></div>
-                        </div>
-                        <div className="gel c29 r3">
-                            <div className="hex-brick h1"></div>
-                            <div className="hex-brick h2"></div>
-                            <div className="hex-brick h3"></div>
-                        </div>
-                        <div className="gel c30 r3">
-                            <div className="hex-brick h1"></div>
-                            <div className="hex-brick h2"></div>
-                            <div className="hex-brick h3"></div>
-                        </div>
-                        <div className="gel c31 r3">
-                            <div className="hex-brick h1"></div>
-                            <div className="hex-brick h2"></div>
-                            <div className="hex-brick h3"></div>
-                        </div>
-                        <div className="gel c32 r3">
-                            <div className="hex-brick h1"></div>
-                            <div className="hex-brick h2"></div>
-                            <div className="hex-brick h3"></div>
-                        </div>
-                        <div className="gel c33 r3">
-                            <div className="hex-brick h1"></div>
-                            <div className="hex-brick h2"></div>
-                            <div className="hex-brick h3"></div>
-                        </div>
-                        <div className="gel c34 r3">
-                            <div className="hex-brick h1"></div>
-                            <div className="hex-brick h2"></div>
-                            <div className="hex-brick h3"></div>
-                        </div>
-                        <div className="gel c35 r3">
-                            <div className="hex-brick h1"></div>
-                            <div className="hex-brick h2"></div>
-                            <div className="hex-brick h3"></div>
-                        </div>
-                        <div className="gel c36 r3">
-                            <div className="hex-brick h1"></div>
-                            <div className="hex-brick h2"></div>
-                            <div className="hex-brick h3"></div>
-                        </div>
-                        <div className="gel c37 r3">
-                            <div className="hex-brick h1"></div>
-                            <div className="hex-brick h2"></div>
-                            <div className="hex-brick h3"></div>
-                        </div>
+                    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2'>
+                        {
+                            contents.map(content => (
+                                <div key={content._id}>
+                                    <div className="card bg-base-100 shadow-xl image-full">
+                                        <figure><img src={content.img} alt="Shoes" /></figure>
+                                        <div className="card-body flex flex-col justify-center items-center my-auto">
+                                            <h2 className="text-white text-3xl">{content.title}</h2>
+                                            <p>If a dog chews shoes whose shoes does he choose?</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            ))
+                        }
                     </div>
                 </div>
             </div>
