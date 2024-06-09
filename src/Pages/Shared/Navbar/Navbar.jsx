b7d4ef086c8291340417e0ce96b45f888fb1d48e
TouhedurRahman <86222384+TouhedurRahman@users.noreply.github.com>
2024-06-09 11:50:37 +0600

    searchbar added in navbar

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -1,11 +1,23 @@
-import { useEffect, useState } from "react";
-import { ImSearch } from "react-icons/im";
+import { useEffect, useRef, useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import useProducts from "../../../Hooks/useProducts";
 import { TiArrowSortedDown } from "react-icons/ti";
-import { Link } from "react-router-dom";
+import { ImSearch } from "react-icons/im";
+import { RxCross2 } from "react-icons/rx";
 import './Navbar.css';
 
 const Navbar = () => {
+    const [products] = useProducts();
+
     const [isScrolled, setIsScrolled] = useState(false);
+
+    const [searchTerm, setSearchTerm] = useState('');
+    const [searchResults, setSearchResults] = useState([]);
+    const [searchText, setSearchText] = useState("");
+
+    const [selectedIndex, setSelectedIndex] = useState(-1);
+    const navigate = useNavigate();
+    const modalBoxRef = useRef(null);
 
     useEffect(() => {
         const handleScroll = () => {
@@ -13,12 +25,106 @@ const Navbar = () => {
             const scrolled = scrollTop > 0;
             setIsScrolled(scrolled);
         };
+        // Add scroll event listener when component mounts
         window.addEventListener('scroll', handleScroll);
 
+        // Remove scroll event listener when component unmounts
         return () => {
             window.removeEventListener('scroll', handleScroll);
         };
     }, []);
+
+    useEffect(() => {
+        // Filter products based on search term
+        const filtered = products.filter(product =>
+            product.forSearch.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+        setSearchResults(filtered);
+    }, [searchTerm, products]);
+
+    const handleSearch = (e) => {
+        setSearchTerm(e.target.value);
+        setSearchText(e.target.value);
+    };
+
+    const handleProductClick = () => {
+        setSearchTerm('');
+        setSearchText('');
+    };
+
+    const location = useLocation();
+
+    useEffect(() => {
+        const closeOtherDetails = (details) => {
+            const allDetails = document.querySelectorAll('details');
+            allDetails.forEach(otherDetails => {
+                if (otherDetails !== details) {
+                    otherDetails.removeAttribute('open');
+                }
+            });
+        };
+
+        const detailsElements = document.querySelectorAll('details');
+        detailsElements.forEach(details => {
+            const links = details.querySelectorAll('.dropdown-link');
+            links.forEach(link => {
+                link.addEventListener('click', (event) => {
+                    event.stopPropagation(); // Prevent details from closing
+                });
+            });
+
+            details.addEventListener('toggle', (event) => {
+                if (event.target.open) {
+                    closeOtherDetails(details);
+                }
+            });
+        });
+
+        // Cleanup event listeners on component unmount
+        return () => {
+            detailsElements.forEach(details => {
+                const links = details.querySelectorAll('.dropdown-link');
+                links.forEach(link => {
+                    link.removeEventListener('click', () => { });
+                });
+                details.removeEventListener('toggle', () => { });
+            });
+        };
+    }, [location]);
+
+    useEffect(() => {
+        const maxHeight = 80; // Max height in vh
+        const minHeight = 13; // Height to fit only the search bar
+        const itemHeight = 6; // Height per item in vh
+
+        const calculatedHeight = searchText.length === 0
+            ? minHeight
+            : Math.min(maxHeight, minHeight + searchResults.length * itemHeight);
+
+        modalBoxRef.current.style.height = `${calculatedHeight}vh`;
+    }, [searchResults, searchText]);
+
+    useEffect(() => {
+        const handleKeyDown = (event) => {
+            if (searchResults.length > 0) {
+                if (event.key === 'ArrowDown') {
+                    setSelectedIndex((prevIndex) => (prevIndex + 1) % searchResults.length);
+                } else if (event.key === 'ArrowUp') {
+                    setSelectedIndex((prevIndex) => (prevIndex - 1 + searchResults.length) % searchResults.length);
+                } else if (event.key === 'Enter' && selectedIndex >= 0) {
+                    navigate(`/products/${searchResults[selectedIndex].category}/${searchResults[selectedIndex]._id}`);
+                    document.getElementById('search_modal').close();
+                    setSearchTerm('');
+                    setSearchText('');
+                }
+            }
+        };
+
+        document.addEventListener('keydown', handleKeyDown);
+        return () => {
+            document.removeEventListener('keydown', handleKeyDown);
+        };
+    }, [searchResults, selectedIndex, navigate]);
 
     return (
         <div className="bg-white text-[#0B5F82] font-mono font-bold">
@@ -199,6 +305,67 @@ const Navbar = () => {
                     </div>
                 </div>
             </div>
+
+            {/* search modal */}
+            <dialog id="search_modal" className="modal">
+                <div
+                    id="modal_box"
+                    ref={modalBoxRef}
+                    className="modal-box bg-gray-50 rounded-lg shadow-xl overflow-y-auto transition-all duration-500 ease-in-out"
+                >
+                    <div>
+                        <div className="flex justify-center">
+                            <div className='flex justify-center items-center border-2 border-[#0B5F82] p-3 rounded-l-lg bg-[#0B5F82] text-white font-extrabold text-shadow-xl'>
+                                <ImSearch />
+                            </div>
+                            <input
+                                type="text"
+                                placeholder="Search products"
+                                value={searchTerm}
+                                onChange={(e) => {
+                                    handleSearch(e);
+                                    setSearchText(e.target.value);
+                                    setSelectedIndex(-1); // Reset selection when search text changes
+                                }}
+                                className="border-2 border-[#0B5F82] border-r-0 px-3 py-2 focus:outline-none focus:ring-[#0B5F82] transition duration-300"
+                            />
+                            <div className='flex justify-center items-center bg-white border-2 border-[#0B5F82] border-l-0 p-3 rounded-r-lg text-[#0B5F82] font-extrabold text-shadow-xl '>
+                                <RxCross2
+                                    className="hover:text-white hover:bg-red-600 rounded-lg cursor-pointer"
+                                    onClick={() => {
+                                        document.getElementById('search_modal').close();
+                                        setSearchTerm('');
+                                        setSearchText('');
+                                    }}
+                                />
+                            </div>
+                        </div>
+                        <div className="relative lg:px-3 z-10">
+                            {searchResults.length > 0 && searchText !== "" && (
+                                <div className="absolute top-full left-0 w-full bg-white shadow-lg rounded mt-2 z-10 text-black font-normal">
+                                    <ul className="divide-y divide-[#0B5F82]">
+                                        {searchResults.map((product, index) => (
+                                            <li
+                                                key={product.id}
+                                                className={`py-2 px-3 cursor-pointer transition-all duration-300 ${selectedIndex === index ? 'bg-[#0B5F82] text-white font-bold' : 'hover:bg-[#0B5F82] hover:text-white hover:font-bold'
+                                                    }`}
+                                                onClick={() => {
+                                                    handleProductClick();
+                                                    document.getElementById('search_modal').close();
+                                                }}
+                                            >
+                                                <Link to={`/products/${product.category}/${product._id}`}>
+                                                    {product.forSearch}
+                                                </Link>
+                                            </li>
+                                        ))}
+                                    </ul>
+                                </div>
+                            )}
+                        </div>
+                    </div>
+                </div>
+            </dialog>
         </div>
     );
 };
