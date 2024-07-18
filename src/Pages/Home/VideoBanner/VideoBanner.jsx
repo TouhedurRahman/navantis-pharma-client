b3b17e6bd56ced6310ec7f0898efc7b0babeafb9
TouhedurRahman <86222384+TouhedurRahman@users.noreply.github.com>
2024-07-18 13:58:15 +0600

    home banner video location updated

--- src/Pages/Home/VideoBanner/VideoBanner.jsx
+++ src/Pages/Home/VideoBanner/VideoBanner.jsx
@@ -3,7 +3,7 @@ const VideoBanner = () => {
         <div className="w-full h-screen overflow-hidden">
             <video
                 className="absolute top-0 left-0 w-full h-full object-cover"
-                src='src/assets/home-bnanner-video.mp4'
+                src='/home-bnanner-video.mp4'
                 autoPlay
                 loop
                 muted
