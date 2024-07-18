408dbb49dce5c2af8d884a919b8bd4a9c1a11f8a
TouhedurRahman <86222384+TouhedurRahman@users.noreply.github.com>
2024-07-18 16:05:33 +0600

    banner picture with text updated

--- src/Components/BannerPicText/BannerPicText.jsx
+++ src/Components/BannerPicText/BannerPicText.jsx
@@ -2,7 +2,7 @@ const BannerPicText = ({ img, title }) => {
     return (
         <div className="mt-20 mb-36 flex flex-col-reverse md:flex-row-reverse lg:justify-between items-center rounded-lg">
             <div className="w-full md:w-1/2 p-5 flex justify-center items-center">
-                <p className="font-bold text-5xl">
+                <p className="font-bold text-5xl text-center">
                     {title}
                 </p>
             </div>
