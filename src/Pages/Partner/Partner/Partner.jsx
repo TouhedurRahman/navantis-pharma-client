b9136e46dc92f99d71e989009ec89e765f934a1a
TouhedurRahman <86222384+TouhedurRahman@users.noreply.github.com>
2024-07-28 16:52:21 +0600

    partner and click to top design updated

--- src/Pages/Partner/Partner/Partner.jsx
+++ src/Pages/Partner/Partner/Partner.jsx
@@ -4,12 +4,11 @@ import LocationFooter from "../../../Components/LocationFooter/LocationFooter";
 const Partner = () => {
     return (
         <div>
+            <BannerOnlyText
+                title={"Our Partner"}
+            ></BannerOnlyText>
             <div className="w-[93%] lg:w-[90%] mx-auto">
-                <BannerOnlyText
-                    title={"Our Partner"}
-                ></BannerOnlyText>
-
-                <div className='mx-3 lg:w-[90%] lg:mx-auto mb-36'>
+                <div className='lg:mx-auto mb-36'>
                     <div>
                         <div className="overflow-x-auto">
                             <table className="hidden md:table">
