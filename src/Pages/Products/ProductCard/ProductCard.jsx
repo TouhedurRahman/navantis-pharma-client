83a5eae44a0d3e6f6443535f4a5ee8705754f316
TouhedurRahman <86222384+TouhedurRahman@users.noreply.github.com>
2024-07-30 11:28:50 +0600

    product card updated

--- src/Pages/Products/ProductCard/ProductCard.jsx
+++ src/Pages/Products/ProductCard/ProductCard.jsx
@@ -2,23 +2,23 @@ import { Link } from "react-router-dom";
 
 const ProductCard = ({ product }) => {
     return (
-        <div className="flex justify-start items-center border border-gray-500">
-            <div className="w-[20%] h-[48] flex justify-center items-center">
+        <div className="flex flex-col lg:flex-row lg:justify-start items-center border border-gray-500">
+            <div className="w-[100%] lg:w-[20%] h-[48] flex justify-center items-center">
                 <img
                     src={product.imageURL}
                     alt="Loading..."
                 />
             </div>
-            <div className="w-[70%] mx-3 py-2">
-                <p className="font-bold">
+            <div className="lg:w-[70%] mx-2 lg:mx-3 py-5 lg:py-2">
+                <p className="font-bold text-center lg:text-justify">
                     {product.forSearch}
                 </p>
-                <p className="text-justify">
+                <p className="text-center lg:text-justify">
                     {product.description.slice(0, 40)}... <Link to={`/products/${product.category}/${product._id}`}
                         className="text-blue-500 hover:link cursor-pointer"
                     >more</Link>
                 </p>
-                <div className="flex justify-end items-center">
+                <div className="flex justify-center lg:justify-end items-center pt-5 lg:pt-0">
                     <Link
                         to={`/products/${product.category}/${product._id}`}
                         className="mt-2 px-5 py-2 border-2 border-[#080567] text-[#080567] rounded hover:bg-[#080567] hover:text-white hover:border-white"
